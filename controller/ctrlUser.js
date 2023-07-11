894619641f16268072e1bc0674610eb4ec2a70bd
AdrianSiwek <105585774+AdrianSiwek@users.noreply.github.com>
2023-07-11 22:55:26 +0200

    Update ctrlUser.js

--- controller/ctrlUser.js
+++ controller/ctrlUser.js
@@ -46,7 +46,34 @@ const registration = async (req, res, next) => {
 };
 
 
-c
+const setAvatar = async (req, res, next) => {
+  try {
+    const { email } = req.user;
+    const { path: tempName, originalname } = req.file;
+    const fileName = path.join(storeAvatar, originalname);
+    await fs.rename(tempName, fileName);
+
+    const img = await Jimp.read(fileName);
+    await img.autocrop().cover(250, 250).quality(60).writeAsync(fileName);
+
+    await fs.rename(
+      fileName,
+      path.join(process.cwd(), "public/avatars", originalname)
+    );
+
+    const avatarURL = path.join(process.cwd(), "public/avatars", originalname);
+    const cleanAvatarURL = avatarURL.replace(/\\/g, "/");
+
+    const user = await service.updateAvatar(email, cleanAvatarURL);
+    res.status(200).json({
+      data: user,
+      message: "File loaded successfully",
+    });
+  } catch (error) {
+    next(error);
+    return res.status(500).json({ message: "Server error" });
+  }
+};
 
 const login = async (req, res, next) => {
   const { email, password } = req.body;
