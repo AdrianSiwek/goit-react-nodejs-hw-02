1a67ea10cdfb8e336ec190c20851bdae1e4567d5
AdrianSiwek <105585774+AdrianSiwek@users.noreply.github.com>
2023-07-11 13:11:18 +0200

    update package.json and service

--- api/index.js
+++ api/index.js
@@ -1,21 +1,18 @@
 const express = require("express");
 const router = express.Router();
+
 const ctrlContact = require("../controller/ctrlContact");
 const ctrlUser = require("../controller/ctrlUser");
 const ctrlAuth = require("../controller/auth");
+
 
 router.get("/contacts", ctrlAuth.auth, ctrlContact.get);
 
 router.get("/contacts/:id", ctrlContact.getById);
 
-router.post("/contacts/", ctrlContact.create);
-
-router.put("/contacts/:id", ctrlContact.update);
-
 router.patch("/contacts/:id/favorite", ctrlContact.updateStatus);
 
 router.delete("/contacts/:id", ctrlContact.remove);
-
 
 router.post("/users/signup", ctrlUser.registration);
 
