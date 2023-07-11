8b88386ed727456bb1654d2cd277f302a5d9d6b7
AdrianSiwek <105585774+AdrianSiwek@users.noreply.github.com>
2023-07-11 22:22:20 +0200

    Update index.js

--- api/index.js
+++ api/index.js
@@ -4,22 +4,34 @@ const router = express.Router();
 const ctrlContact = require("../controller/ctrlContact");
 const ctrlUser = require("../controller/ctrlUser");
 const ctrlAuth = require("../controller/auth");
+const multer = require("multer");
+const path = require("path");
+const storeAvatar = path.join(process.cwd(), "tmp");
 
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, storeAvatar);
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.originalname);
+    },
+    limits: {
+        fileSize: 1048576,
+    },
+});
+const upload = multer({ storage });
 
 router.get("/contacts", ctrlAuth.auth, ctrlContact.get);
-
-router.get("/contacts/:id", ctrlContact.getById);
-
-router.patch("/contacts/:id/favorite", ctrlContact.updateStatus);
-
-router.delete("/contacts/:id", ctrlContact.remove);
-
-router.post("/users/signup", ctrlUser.registration);
-
-router.post("/users/login", ctrlUser.login);
 
 router.get("/users/logout", ctrlAuth.auth, ctrlUser.logout);
 
 router.get("/users/current", ctrlAuth.auth, ctrlUser.current);
+
+router.patch(
+    "/users/avatars",
+    upload.single("avatar"),
+    ctrlAuth.auth,
+    ctrlUser.setAvatar
+);
 
 module.exports = router;
