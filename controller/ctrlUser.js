63840bd48016ac991d52198b02e1c593f8dbb919
AdrianSiwek <105585774+AdrianSiwek@users.noreply.github.com>
2023-07-11 22:46:43 +0200

    Merge branch 'master' of https://github.com/AdrianSiwek/goit-react-nodejs-hw-02 into  hw05-avatars

--- controller/ctrlUser.js
+++ controller/ctrlUser.js
@@ -1,6 +1,12 @@
 const service = require("../service");
 const jwt = require("jsonwebtoken");
 const User = require("../service/schemas/user");
+const gravatar = require("gravatar");
+
+const path = require("path");
+const storeAvatar = path.join(process.cwd(), "tmp");
+const fs = require("fs/promises");
+const Jimp = require("jimp");
 
 require("dotenv").config();
 const SECRET = process.env.SECRET;
@@ -8,6 +14,12 @@ const SECRET = process.env.SECRET;
 const registration = async (req, res, next) => {
   const { email, password } = req.body;
   const user = await service.getUserByEmail(email);
+
+  const avatarURL = gravatar.url(
+    email,
+    { s: "250", r: "pg", d: "retro" },
+    true
+  );
 
   if (user) {
     return res.status(409).json({
@@ -18,7 +30,7 @@ const registration = async (req, res, next) => {
     });
   }
   try {
-    const newUser = new User({ email });
+    const newUser = new User({ email, avatarURL });
     newUser.setPassword(password);
     await newUser.save();
     res.json({
@@ -30,6 +42,36 @@ const registration = async (req, res, next) => {
     });
   } catch (err) {
     next(err);
+  }
+};
+
+
+const setAvatar = async (req, res, next) => {
+  try {
+    const { email } = req.user;
+    const { path: tempName, originalname } = req.file;
+    const fileName = path.join(storeAvatar, originalname);
+    await fs.rename(tempName, fileName);
+
+    const img = await Jimp.read(fileName);
+    await img.autocrop().cover(250, 250).quality(60).writeAsync(fileName);
+
+    await fs.rename(
+      fileName,
+      path.join(process.cwd(), "public/avatars", originalname)
+    );
+
+    const avatarURL = path.join(process.cwd(), "public/avatars", originalname);
+    const cleanAvatarURL = avatarURL.replace(/\\/g, "/");
+
+    const user = await service.updateAvatar(email, cleanAvatarURL);
+    res.status(200).json({
+      data: user,
+      message: "File loaded successfully",
+    });
+  } catch (error) {
+    next(error);
+    return res.status(500).json({ message: "Server error" });
   }
 };
 
@@ -101,4 +143,5 @@ module.exports = {
   login,
   logout,
   current,
+  setAvatar,
 };
